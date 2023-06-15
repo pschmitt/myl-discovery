9a99e96cdf85aa19436650f5b8910db3c9d53211
Philipp Schmitt <philipp@schmitt.co>
2023-06-15 08:30:35 +0200

    Export more funcs

--- myldiscovery/__init__.py
+++ myldiscovery/__init__.py
@@ -1,4 +1,14 @@
-from .discovery import autodiscover, autodiscover_exchange
+from .discovery import (autodiscover, autodiscover_autoconfig,
+                        autodiscover_exchange, autodiscover_port_scan,
+                        autodiscover_srv, autodiscover_txt)
 from .main import main
 
-__all__ = ["main", "autodiscover", "autodiscover_exchange"]
+__all__ = [
+    "main",
+    "autodiscover",
+    "autodiscover_autoconfig",
+    "autodiscover_exchange",
+    "autodiscover_port_scan",
+    "autodiscover_srv",
+    "autodiscover_txt",
+]
